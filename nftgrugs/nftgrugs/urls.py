7da28009735856083a5bbc1f1b65cf98d7927a5f
sad-descartes <97770703+sad-descartes@users.noreply.github.com>
2022-01-14 22:32:48 -0600

    Update urls.py

--- nftgrugs/nftgrugs/urls.py
+++ nftgrugs/nftgrugs/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('NFT/', include('NFT.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
+
