ad8d9022ca69bffe6b3351c2e2cbfcff76a9321f
Wie Bitte <tf.jp@protonmail.com>
2022-04-03 16:42:36 -0500

    n

---
+++ JSTikTakToe/index.js
@@ -0,0 +1,21 @@
+function xonigga() {
+
+}
+
+function tiktaktoe() {
+  let a = ["", "", ""];
+  let b = ["", "", ""];
+  let c = ["", "", ""];
+  let d = ["", "", ""];
+  let e = ["", "", ""];
+  let f = ["", "", ""];
+  let x = ["", "", ""];
+  let y = ["", "", ""];
+    if (a||b||c||d||e||f||x||y == ["x", "x", "x"] || a||b||c||d||e||f||x||y == ["o", "o", "o"]) {
+      gameover();}
+    else return;
+}
+
+function gameover(){
+
+}
